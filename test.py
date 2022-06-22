0a131f058ffe4753e4fb51c134f685f6c87d3ebe
marsvo1ta <euromobilco@gmail.com>
2022-06-22 10:07:06 +0300

    basecase

--- test.py
+++ test.py
@@ -1,34 +1,13 @@
-from appium import webdriver
+
 from appiumbase import BaseCase
+from appium.webdriver.common.mobileby import MobileBy
 
 
-desired_cap = {
-    # Set your access credentials
-    "browserstack.user" : "antonnemchynov_xdXhmt",
-    "browserstack.key" : "g2w5qYhQYBnCukypqeks",
-
-    # Set URL of the application under test
-    "app" : "bs://a3026cfa929f43641848069d01d4d5b8246dc449",
-
-    # Specify device and os_version for testing
-    "device" : "Xiaomi Redmi Note 7",
-    "os_version" : "9.0",
-    "device" : "Samsung Galaxy S8 Plus",
-    "os_version" : "9.0",
-    "automationName" : "UiAutomator2",
-    
-    # Set other BrowserStack capabilities
-    "project" : "First Python project", 
-    "build" : "Python Android",
-    "name" : "first_test"
-}
 
 
-def test_two():
-    assert 1==1
+class Velvot(BaseCase):
 
-def test_bro():
-    driver = webdriver.Remote(
-    command_executor="http://hub-cloud.browserstack.com/wd/hub", 
-    desired_capabilities=desired_cap)
-    driver.quit()
+    def test_create_account(self):
+        self.click('com.velvot.android:id/onBoardingCreateBtn', MobileBy.ID)
+        self.set_text('com.velvot.android:id/registrationFirstStepEmailTiEt','22', MobileBy.ID)
+        self.set_text('com.velvot.android:id/registrationFirstStepPasswordTiEt', MobileBy.ID)
