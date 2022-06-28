1a0f098fa2a45ec05802eb1d0a8e9b36c6df7824
marsvo1ta <euromobilco@gmail.com>
2022-06-28 12:25:46 +0300

    bro

---
+++ test.py
@@ -0,0 +1,13 @@
+
+from appiumbase import BaseCase
+from appium.webdriver.common.mobileby import MobileBy
+import pytest
+
+
+
+class Velvot(BaseCase):
+
+    def test_create_account(self):
+        self.click('com.velvot.android:id/onBoardingCreateBtn', MobileBy.ID)
+        self.set_text('com.velvot.android:id/registrationFirstStepEmailTiEt','22', MobileBy.ID)
+        self.set_text('com.velvot.android:id/registrationFirstStepPasswordTiEt', MobileBy.ID)
