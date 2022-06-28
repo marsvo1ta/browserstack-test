bbcc1cc3943a1281e7df150b703d9a70ea06e21e
marsvo1ta <euromobilco@gmail.com>
2022-06-28 11:27:48 +0300

    untrack files contained in the .gitignore file

---
+++ browserstack_caps.py
@@ -0,0 +1,12 @@
+desired_cap = {
+    
+    "browserstack.user" : "antonnemchynov_xdXhmt",
+    "browserstack.key" : "g2w5qYhQYBnCukypqeks",
+    "app" : "bs://a3026cfa929f43641848069d01d4d5b8246dc449",
+    "device" : "Xiaomi Redmi Note 7",
+    "os_version" : "9.0",
+    "automationName" : "UiAutomator2",
+    "project" : "First Python project", 
+    "build" : "Python Android",
+    "name" : "browserstack_test"
+}
